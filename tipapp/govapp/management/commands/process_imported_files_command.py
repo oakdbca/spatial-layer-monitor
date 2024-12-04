62ef3df9725d362ffcf5b1e9d0218070a3d2d2a7
Juan Camilo Achury Bueno <jcachuryb@outlook.com>
2024-12-04 15:26:31 +0800

    Added the command class tha extracts the contents

---
+++ tipapp/govapp/management/commands/process_imported_files_command.py
@@ -0,0 +1,29 @@
+"""Kaartdijin Boodja Catalogue Scan Management Command."""
+
+
+# Third-Party
+import os
+import logging
+import subprocess
+from django.core.management import base
+from datetime import datetime
+# Local
+from govapp import settings
+from govapp.imports_processor import ImportsProcessor
+logger = logging.getLogger(__name__)
+
+
+class Command(base.BaseCommand):
+    """Read Imported Thermal Files Command."""
+    # Help string
+    help = "Processes Imported Thermal Files"  # noqa: A003
+
+    def handle(self, *args, **kwargs) -> None:
+        """Handles the management command functionality."""
+        # Display information
+        self.stdout.write("Scanning staging area")
+        ImportsProcessor(settings.PENDING_IMPORT_PATH).process_files()
+
+        
+        
+
