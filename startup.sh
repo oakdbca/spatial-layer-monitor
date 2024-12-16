857124eb309342c8c88181222a21ebaafaba4dae
Jason Moore <jmoore@austwa.com>
2024-12-16 12:16:04 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Start the first process
-# env > /etc/.cronenv
-# sed -i 's/\"/\\"/g' /etc/.cronenv
 cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1 > /app/git_hash
 
 if [ $ENABLE_CRON == "True" ];
@@ -17,5 +15,18 @@ fi
 
 fi
 
-echo "System Started"
-/bin/bash
+if [ $ENABLE_WEB == "True" ];
+    then
+echo "Starting Gunicorn"
+# Start the second process
+
+/app/venv/bin/gunicorn tipapp.wsgi --bind :8080 --config /app/gunicorn.ini
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start gunicorn: $status"
+  exit $status
+fi
+else
+   echo "ENABLE_WEB environment vairable not set to True, web server is not starting."
+   /bin/bash
+fi
