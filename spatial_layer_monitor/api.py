93c276c6fea01c26a89d15896c65d8403901dd5a
Juan Camilo Achury Bueno <jcachuryb@outlook.com>
2025-02-05 15:24:18 +0800

    List of historical data in new View

--- spatial_layer_monitor/api.py
+++ spatial_layer_monitor/api.py
@@ -1,7 +1,7 @@
 import threading
 import logging
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpRequest, HttpResponse
 from django.views import View
 from rest_framework import routers, serializers, viewsets
 from rest_framework.decorators import api_view, permission_classes
@@ -20,8 +20,52 @@ def mock_kmi_service(request):
     
 
 @api_view(['POST'])
-@permission_classes([IsInOfficersGroup])
 def run_batch_import(request):
     x = threading.Thread(target=run_check_all_layers)
     x.run()
     return JsonResponse({'message': 'Success'}, status=200)
+
+
+@api_view(['POST'])
+def test_xml_request(request: HttpRequest):
+    print(request.content_type)
+    print(vars(request.headers))
+    response  =  HttpResponse()
+    response.headers = {
+        'content-type': "text/xml"
+    }
+    response.status_code = 201
+    return response
+
+
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.negotiation import BaseContentNegotiation
+
+class IgnoreClientContentNegotiation(BaseContentNegotiation):
+    def select_parser(self, request, parsers):
+        """
+        Select the first parser in the `.parser_classes` list.
+        """
+        return parsers[0]
+
+    def select_renderer(self, request, renderers, format_suffix):
+        """
+        Select the first renderer in the `.renderer_classes` list.
+        """
+        return (renderers[0], renderers[0].media_type)
+class NoNegotiationView(APIView):
+    """
+    An example view that does not perform content negotiation.
+    """
+    content_negotiation_class = IgnoreClientContentNegotiation
+
+    def get(self, request, format=None):
+        print(request.content_type)
+        print(vars(request.headers))
+        response  =  HttpResponse()
+        response.headers = {
+        'content-type': "text/xml"
+        }
+        return response
